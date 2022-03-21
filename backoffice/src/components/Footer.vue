f8ba1bba2bccb0cee814fcfddcada05d77e4a5ef
Tania Olivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-21 16:54:30 +0100

    ajout des components (header et footer)

---
+++ backoffice/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        @Reunionou-BackOffice - 2021/2022
+    </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+footer{
+    position: absolute; 
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+    padding: 20px 10px;
+    font-family: system-ui;
+    font-size: 15px;
+    font-weight: bold;
+    color: white;
+    width: 99%;
+    margin-left: -10px;
+    text-align: center;
+}
+</style>
