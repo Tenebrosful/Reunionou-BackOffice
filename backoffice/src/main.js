d2a88880f7c7e06c28954fa7024f7c70358230e8
Tania Olivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-21 17:01:15 +0100

    modification de main.js (ajout axios et components header et footer)

--- backoffice/src/main.js
+++ backoffice/src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false
+
+Vue.component("Header", () => import ("@/components/Header.vue"));
+
+Vue.component("Footer", () => import ("./components/Footer.vue"));
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    router,
+    render: h => h(App)
+}).$mount('#app')
