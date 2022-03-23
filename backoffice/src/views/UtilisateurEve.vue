c448d7087f9682d685bca3bdf1f9ffd20f88b80e
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-23 19:01:57 +0100

    implementation des api de la vue des évenémenets d'un utilisateur

--- backoffice/src/views/UtilisateurEve.vue
+++ backoffice/src/views/UtilisateurEve.vue
@@ -2,36 +2,42 @@
   <section>
     <Header />
     <div id="data">
-      <div id="listeevenements">
+      <div id="event-buttons">
+        <button type="submit" v-on:click="getSelfEvents()">Ses événements</button>
+        <button type="submit" v-on:click="getJoinedEvents()">Participation aux événements</button>
+      </div>
+      <div id="listeevenement">
         <div class="unevenement" v-for="evenement in evenements">
           <div class="date">
-            <h1>06</h1>
-            <h1>JUIN</h1>
+            <h1>{{jour(evenement.date)}}</h1>
+            <h1>{{mois(evenement.date)}}</h1>
           </div>
           <div class="description">
-            <h1>Pearl Jam</h1>
-            <p><i class="fas fa-map-marker-alt fa-lg"></i>{{evenement.address.street}}</p>
-            <p><i class="fas fa-user-crown fa-lg"></i>{{evenement.name}}</p>
-            <p><i class="fas fa-male fa-lg"></i>100 participants</p>
+            <h1>{{evenement.title}}</h1>
+            <p><i class="fas fa-map-marker-alt fa-lg"></i>{{evenement.coords.address}}</p>
+            <div v-if="owner != null">
+              <p><i class="fas fa-user-crown fa-lg"></i>{{owner.username}}</p>
+            </div>
+            <div v-else>
+              <p><i class="fas fa-user-crown fa-lg"></i>{{evenement.owner.username}}</p>
+            </div>
+            <p><i class="fas fa-male fa-lg"></i>{{evenement.comingParticipant}} participant(s)</p>
+            <p>{{evenement.description}}</p>
           </div>
           <div class="autre">
             <p><i class="fas fa-trash fa-lg"></i></p>
-            <p v-on:click="getCommentaires(evenement.id)"><i class="fas fa-comment fa-lg"></i></p>
+            <p><i class="fas fa-comment fa-lg" v-on:click="getCommentaires(evenement.id)"></i></p>
           </div>
           <div class="commentaires">
-            <div v-if="id == evenement.id">
-              <div class="scroller"  v-if="afficher">
-                <p><strong>{{evenement.name}} : </strong>{{evenement.name}}</p>
-                <p><strong>{{evenement.name}} : </strong>{{evenement.name}}</p>
-                <p><strong>{{evenement.name}} : </strong>{{evenement.name}}</p>
-                <p><strong>{{evenement.name}} : </strong>{{evenement.name}}</p>
-                <p><strong>{{evenement.name}} : </strong>{{evenement.name}}</p>
-                <p><strong>{{evenement.name}} : </strong>{{evenement.name}}</p>
+            <div class="scroller" v-if="afficher">
+              <div v-for="commentaire in commentaires">
+                <div v-if="evenement.id == commentaire.event_id">
+                    <p><strong>{{commentaire.author.username}} : </strong>{{commentaire.message}}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        
       </div>
     </div>
     <Footer />
@@ -40,39 +46,79 @@
 
 <script>
 import axios from 'axios';
-import StackModal from '@innologica/vue-stackable-modal';
+import moment from "moment";
 
 export default {
     name: 'Evenements',
     data() {
         return {
           evenements: "",
-          comments: "",
+          commentaires: "",
           afficher: false,
-          id: 0
+          id: this.$route.params.id,
+          owner: ""
         }
     },
     methods: {
-       getCommentaires(id){
+        jour(value) {
+            moment.locale('fr');
+            return moment(String(value)).format('DD');
+        },
+        mois(value) {
+            moment.locale('fr');
+            return moment(String(value)).format('MMMM');
+        },
+        getCommentaires(id){
          axios
-            .get("https://jsonplaceholder.typicode.com/users/" + id)
+            .get("http://docketu.iutnc.univ-lorraine.fr:62460/api/event/" + id + '/comments?embedAuthor=true')
             .then(response => {
-                this.comments = response.data;
+                this.commentaires = response.data.comments;
                 this.afficher = true;
-                this.id = id;
-                console.log(this.comments)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+       },
+       getOwner(id){
+         axios
+            .get("http://docketu.iutnc.univ-lorraine.fr:62460/api/user/" + id)
+            .then(response => {
+                this.owner = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+       },
+       getSelfEvents(){
+         axios
+            .get("http://docketu.iutnc.univ-lorraine.fr:62460/api/user/" + this.id + '/self-event?participants=true')
+            .then(response => {
+                this.getOwner(this.id);
+                this.evenements = response.data.events;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+       },
+       getJoinedEvents(){
+         axios
+            .get("http://docketu.iutnc.univ-lorraine.fr:62460/api/user/" + this.id + '/joined-event?participants=true&embedOwner=true')
+            .then(response => {
+                this.owner = null;
+                this.evenements = response.data.events;
             })
             .catch(error => {
                 console.log(error);
             });
        }
+      
     },
     created(){
          axios
-            .get("https://jsonplaceholder.typicode.com/users")
+            .get("http://docketu.iutnc.univ-lorraine.fr:62460/api/user/" + this.id + '/self-event?participants=true')
             .then(response => {
-                this.evenements = response.data;
-                console.log(this.evenements)
+                this.getOwner(this.id);
+                this.evenements = response.data.events;
             })
             .catch(error => {
                 console.log(error);
@@ -82,6 +128,31 @@ export default {
 }
 </script>
 <style lang="scss">
+#event-buttons{
+  padding-top: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#event-buttons button{
+  background-color: darkslateblue;
+  border: 1px solid darkslateblue;
+  border-radius: 5px;
+  color: white;
+  font-weight: bold;
+  padding: 15px 30px;
+  font-size: 20px;
+}
+
+#event-buttons button:hover{
+  background-color: slateblue;
+}
+
+#event-buttons button:nth-of-type(1){
+  margin-right: 50px;
+}
+
 .scroller {
   width: 100%;
   height: 180px;
@@ -92,29 +163,22 @@ export default {
 
 .commentaires{
   background-color: slateblue;
-  height: 92%;
-  width: 78%;
-  padding: 10px 50px;
   color: white;
-
+  padding: 10px 20px;
 }
 
-body{
-  font-family: system-ui;
-}
-
-#listeevenements{
-  padding-top: 120px;
+#listeevenement{
+  padding-top: 40px;
   padding-bottom: 50px;
 }
 
 .unevenement{
   border: 5px solid slateblue;
   display: grid;
-  grid-template-columns: 0.4fr 0.6fr 0.2fr 0.8fr;
+  grid-template-columns: 0.4fr 1fr 0.1fr 0.6fr;
   grid-column-gap: 50px;
   margin: 20px auto 50px auto;
-  width: 70%;
+  width: 85%;
 }
 .fa-user-crown{
   color: gray;
@@ -131,6 +195,10 @@ body{
   color: gray;
   margin-left: 5px;
   margin-right: 25px;
+}
+
+.description p:nth-of-type(4){
+  text-align: justify;
 }
 
 .date{
@@ -158,18 +226,17 @@ body{
   flex-direction: column;
 }
 
-.fa-comment{
+.autre .fa-comment{
   color: gray;
-  padding: 20px 20px;
-
+  padding: 20px 5px 20px 20px;
 }
 
 .autre p:hover{
   background-color: darkgray;
   
 }
-.fa-trash{
+.autre .fa-trash{
     color: gray;
-    padding: 20px 20px;
+    padding: 20px 5px 20px 20px;
 }
 </style>
